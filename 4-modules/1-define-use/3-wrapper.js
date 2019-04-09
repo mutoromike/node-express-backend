9facbcebc3f06e5aa0d4d37e8f12bbc423f4a51e
strongestavenger <mikemutoro@gmail.com>
2019-04-09 13:40:13 +0300

    modify modules

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,8 @@
-console.log(arguments);
+// function(exports, module, require, __filename, __dirname) {
+
+// console.log(arguments);
+exports.a = 39;
+module.exports.b = 90;
+// The built in wrapping function also returns the:
+// return module.exports;
+// }  This function is always called before the keywords arguements
