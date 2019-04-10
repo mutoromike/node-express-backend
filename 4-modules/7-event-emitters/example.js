9b38ca5761fec6b3d2904e44cb0e3ee6e3a254fb
strongestavenger <mikemutoro@gmail.com>
2019-04-10 15:52:52 +0300

    work on emmiters

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -2,3 +2,13 @@ const EventEmitter = require('events');
 
 // Streams are Event Emitters
 // process.stdin, process.stdout
+
+const myEmitter = new EventEmitter();
+
+setImmediate(()=>{
+    myEmitter.emit('TEST_EVENT');
+});
+
+myEmitter.on('TEST_EVENT', () => {
+    console.log('he used an axe');
+});
